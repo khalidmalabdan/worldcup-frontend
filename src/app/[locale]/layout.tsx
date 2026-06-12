f0902444cb5617d0e2d1b2d9ebb18a2da6c242fb
khalidmalabdan <kalabdan@alfaisal.edu>
2026-06-12 07:05:55 -0600

    locale

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,17 @@
 import type { ReactNode } from "react";
+import { setRequestLocale } from "next-intl/server";
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function LocaleLayout({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: { locale: string };
+}) {
+  setRequestLocale(params.locale);
+
   return (
-    <html>
+    <html lang={params.locale}>
       <body>{children}</body>
     </html>
   );
