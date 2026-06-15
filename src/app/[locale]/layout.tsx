e3c4c6cd5fd49d89942189fe57f81afb8239f3f4
khalidmalabdan <kalabdan@alfaisal.edu>
2026-06-15 01:10:41 -0600

    fixed front page code 22

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,9 +3,5 @@ export default function LocaleLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+  return <>{children}</>;
 }
