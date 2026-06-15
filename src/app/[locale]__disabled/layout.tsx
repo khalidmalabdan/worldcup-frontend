2ea44b0227437068529cc9e5d569e02fa58d3451
khalidmalabdan <kalabdan@alfaisal.edu>
2026-06-15 00:37:49 -0600

    fixed front page code 14

--- src/app/[locale]__disabled/layout.tsx
+++ src/app/[locale]__disabled/layout.tsx
@@ -1,21 +1,33 @@
-import type { ReactNode } from "react";
-import { setRequestLocale } from "next-intl/server";
+import React from "react";
 
-export default function LocaleLayout({
-  children,
-  params,
-}: {
-  children: ReactNode;
-  params: { locale: string };
-}) {
-  const { locale } = params;
 
-  // Register locale for next-intl
-  setRequestLocale(locale);
-
+export default function LocaleLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang={locale}>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
 }
+
+
+// import type { ReactNode } from "react";
+// import { setRequestLocale } from "next-intl/server";
+
+// export default function LocaleLayout({
+//   children,
+//   params,
+// }: {
+//   children: ReactNode;
+//   params: { locale: string };
+// }) {
+//   const { locale } = params;
+
+//   // Register locale for next-intl
+//   setRequestLocale(locale);
+
+//   return (
+//     <html lang={locale}>
+//       <body>{children}</body>
+//     </html>
+//   );
+// }
